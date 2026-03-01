fb19d59a55057a7a3b2048cca23bc05671a2abfa
minjoon-and <minjoon.and@gmail.com>
2026-03-01 11:28:16 +0900

    test: input, output 확인

--- COTE/current.cpp
+++ COTE/current.cpp
@@ -12,56 +12,9 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
     
-    int N;
-    cin >> N;
-    vector<int> budgets(N);
-    int maxBudget = 0;
-    long long totalRequest = 0;
-
-    for (int i = 0; i < N; i++)
-    {
-        cin >> budgets[i];
-        maxBudget = max(maxBudget, budgets[i]);
-        totalRequest += budgets[i];
-    }
-
-    long long M;
-    cin >> M;
-
-    // 모든 요청을 만족할 수 있는 경우
-    if (totalRequest <= M)
-    {
-        cout << maxBudget << '\n';
-    }
-    else
-    {
-        // 이분탐색으로 최적의 상한액 찾기
-        int left = 0, right = maxBudget;
-        int ans = 0;
-        while (left <= right)
-        {
-            int mid = left + (right - left) / 2;  // mid = 상한액 후보
-
-            // mid를 상한액으로 했을 때 총 배정액 계산
-            long long sum = 0;
-            for (int i : budgets)
-            {
-                sum += (i <= mid) ? i : mid;
-            }
-
-            // 예산 이내면 더 큰 상한액 시도
-            if (sum <= M)
-            {
-                ans = max(ans, mid);
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid - 1;
-            }
-        }
-        cout << ans << '\n';
-    }
+    int test;
+    cin >> test;
+    cout << test;
 
     return 0;
 }
